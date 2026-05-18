65302187a5021f7006c4b1cfce04a08a576c68ff
ZLal <zeta.amrithlal@gmail.com>
2026-05-18 12:16:04 +0530

    UI initial changes

--- src/PaymentGatewayPOC/Program.cs
+++ src/PaymentGatewayPOC/Program.cs
@@ -3,6 +3,7 @@ using PaymentGatewayPOC.Repositories;
 using PaymentGatewayPOC.Repositories.Interfaces;
 using PaymentGatewayPOC.Services;
 using PaymentGatewayPOC.Services.Interfaces;
+using PaymentGatewayPOC.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,6 +22,10 @@ builder.Services.AddScoped<IApplicationService, ApplicationService>();
 builder.Services.AddScoped<IClientService, ClientService>();
 builder.Services.AddScoped<IGatewayService, GatewayService>();
 builder.Services.AddScoped<ITransactionService, TransactionService>();
+
+// Register Blazor components
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
@@ -49,7 +54,22 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // app.UseHsts();
+}
 
 app.MapControllers();
+
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+// app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();
